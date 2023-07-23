39affe995bf3c9f39e00d84b5ec652d98570f227
Joel Carrillo <jocarrillo@copaair.com>
2023-07-23 17:52:18 -0500

    avances2

--- index.js
+++ index.js
@@ -1,42 +1,60 @@
+require('dotenv').config()
 const express = require('express');
+const paypal = require('./paypal.js');
 const supabase = require('@supabase/supabase-js');
 const app = express();
 const port = 3000;
 
 const options = {
   auth: {
-    autoRefreshToken: true,
-    persistSession: true,
-    detectSessionInUrl: true,
+    autoRefreshToken: false,
+    persistSession: false,
+    detectSessionInUrl: false
   }
 }
 const client = supabase.createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY, options);
+
+const retrieveSession = async (req, res, next) => {
+  const { data, error } = await client.auth.getSession();
+  if (data.session) {
+    res.locals.session = data.session.user.user_metadata;
+  } else {
+    res.locals.session = null;
+  }
+  req.session = data.session;
+  next();
+}
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(retrieveSession);
 
 app.get('/', async (req, res) => {
   res.render('pages/index');
-  const { data, error } = await client.auth.getSession();
-  console.log(data, error);
 });
 
-app.get('/login', (req, res) => {
-  res.render('pages/login');
+app.get('/login', async (req, res) => {
+  if (req.session) {
+    res.redirect('/');
+  } else {
+    res.render('pages/login');
+  }
 });
 
 app.post('/login', async (req, res) => {
-  try {
-    let { data, error } = await client.auth.signInWithPassword({
-      email: req.body.email,
-      password: req.body.password,
-    });
-  } catch (err) {
-    console.log('Error authenticating');
-  }
-  res.render('pages/index');
+  let { data: signInData, err } = await client.auth.signInWithPassword({
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  res.redirect('/');
+});
+
+app.get('/logout', async (req, res) => {
+  let { error } = await client.auth.signOut();
+  res.redirect('/');
 });
 
 app.get('/register', (req, res) => {
@@ -75,6 +93,63 @@ app.get('/category/:name', async (req, res) => {
 app.get('/product/:id', async (req, res) => {
   let { data: product, error } = await client.from('product').select('*').eq('id', req.params.id).limit(1).single();
   res.render('pages/product', { product });
+});
+
+app.post('/product/:id', async (req, res) => {
+  if (req.session) { 
+    let userId = req.session.user.id;
+    let productId = req.params.id;
+    const { error } = await client.from('cart-item').insert({user_id: userId, product_id: productId});
+    res.redirect('/cart');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/cart', async (req, res) => {
+  if (req.session) {
+    const { data: cartItems, error } = await client.from('cart-item').select('id, product_id').eq('user_id', req.session.user.id);
+    let products = null;
+    let total = 0;
+    if (cartItems.length > 0) {
+      const productIds = cartItems.map(item => item.product_id);
+      const { data: productItems, productError } = await client.from('product').select('*').in('id', productIds);
+
+      const productItemDict = productItems.reduce((result, item) => {
+        const { id, ...rest } = item;
+        result[id] = rest;
+        return result;
+      }, {});
+      
+      products = cartItems.map(item => {
+        return {cart_item_id: item.id, product_id: item.product_id, ...productItemDict[item.product_id]} 
+      });
+      total = products.reduce((result, item) => {
+        return result + item.price;
+      }, 0).toFixed(2);
+    }
+    res.render('pages/cart', { products, total });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/cart/:id', async (req, res) => {
+  if (req.session) {
+    const { error } = await client.from('cart-item').delete().eq('id', req.params.id);
+    res.redirect('/cart');
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.post('/checkout', async (req, res) => {
+  if (req.session) {
+    const order = await paypal.createOrder(200);
+    res.json(order);
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.listen(port, () => {
---
+++ paypal.js
@@ -0,0 +1,57 @@
+const { PAYPAL_CLIENT_ID, PAYPAL_CLIENT_SECRET, ENVIRONMENT = 'sandbox' } = process.env;
+const baseUrl = {
+  sandbox: "https://api-m.sandbox.paypal.com",
+  production: "https://api-m.paypal.com",
+}
+
+// use the oauth2/token api to get an access token
+function getPaypalAccessToken() {
+  console.log({ 
+    PAYPAL_CLIENT_ID,
+    PAYPAL_CLIENT_SECRET,
+    ENVIRONMENT,
+    baseUrl: baseUrl[ENVIRONMENT],
+   })
+  return fetch(`${baseUrl[ENVIRONMENT]}/v1/oauth2/token`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_CLIENT_SECRET}`).toString('base64')}`,
+    },
+    body: "grant_type=client_credentials",
+  })
+  .then((response) => response.json())
+  .then((data) => data.access_token);
+}
+
+
+// use the orders api to create an order
+async function createOrder(value) {
+  // create accessToken using your clientID and clientSecret
+  // for the full stack example, please see the Standard Integration guide 
+  // https://developer.paypal.com/docs/multiparty/checkout/standard/integrate/
+  const accessToken = await getPaypalAccessToken();
+  return fetch (`${baseUrl[ENVIRONMENT]}/v2/checkout/orders`, {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({
+      "purchase_units": [
+        {
+          "amount": {
+            "currency_code": "USD",
+            "value": value,
+          },
+        }
+      ],
+      "intent": "CAPTURE",
+    })
+  })
+  .then((response) => response.json());
+}
+
+module.exports = {
+  createOrder,
+};
